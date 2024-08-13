a0621053b7cbe3e94df2ccc7c6d265cba3556ae6
Victoria Choi <146795123+victoria4318@users.noreply.github.com>
2024-08-12 21:50:57 -0400

    Update main.py
    checkstyle

--- main.py
+++ main.py
@@ -11,22 +11,25 @@ client = commands.Bot(command_prefix='!', intents=intents)
 
 
 @client.event
-async def on_ready(): # not shown to user just tells creator that bot is ready and loaded to use
+async def on_ready():  # not shown to user just tells creator that bot is ready and loaded to use
     # bot status
-    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name='Signal'))
+    await client.change_presence(status=discord.Status.online,
+                                 activity=discord.Activity(type=discord.ActivityType.listening, name='Signal'))
 
     print("The bot is now ready for use!")
     print("-----------------------------")
-    
+
 
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await client.load_extension(f"cogs.{filename[:-3]}")
 
+
 async def main():
     async with client:
         await load()
         await client.start(BOTTOKEN)
+
 
 asyncio.run(main())
